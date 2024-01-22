c887c165a08d6f0d52d941bed57dee61566c3641
SvdNeo <sanjay.d@neoito.com>
2024-01-22 14:28:48 +0530

    accessed the sort button and created the sortinput array function to access rthe input values

---
+++ script.js
@@ -0,0 +1,15 @@
+const sortButton = document.getElementById("sort");
+
+const sortInputArray = (event) => {
+  event.preventDefault();
+
+  const inputValues = [
+    ...document.getElementsByClassName("values-dropdown")
+  ].map((dropdown) => Number(dropdown.value));
+
+  const sortedValues = inputValues.sort((a, b) => {
+    return a - b;
+  });
+
+  updateUI(sortedValues);
+}
