495b70d639669dca30a718457513a6180c9b2e3e
SvdNeo <sanjay.d@neoito.com>
2024-01-22 14:30:53 +0530

    created the bubblesort function

--- script.js
+++ script.js
@@ -19,4 +19,16 @@ const updateUI = (array = []) => {
       outputValueNode.innerText = num;
     })
   }
+  const bubbleSort = (array) => {
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array.length - 1; j++) {
+        if (array[j] > array[j + 1]) {
+          const temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+        }
+      }
+    }
   
+    return array;
+  }
